57a55cc392b318a6aae179b100bda1f968d6291e
Antonio0512 <antonioboyanov9@gmail.com>
2023-08-12 15:50:59 +0300

    Fixed mistakes in the tests

--- tests/ui/boardgames.test.js
+++ tests/ui/boardgames.test.js
@@ -2,7 +2,7 @@ const { test, expect } = require('@playwright/test');
 
 test('Check boardgames page', async ({ page }) => {
     await page.goto('http://localhost:8080/collection');
-    const list = await page.$('ul');
+    const list = await page.$$('li');
     expect(list).toBeTruthy();
   });
   
